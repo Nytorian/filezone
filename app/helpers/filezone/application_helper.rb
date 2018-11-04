73752ccce33f99d837262f19c02045596d077ac8
Mat <mateusz.michalski@cpoms.co.uk>
2018-11-04 19:52:11 +0000

    Add file area partial

--- app/helpers/filezone/application_helper.rb
+++ app/helpers/filezone/application_helper.rb
@@ -1,4 +1,9 @@
 module Filezone
   module ApplicationHelper
+    def svg(name)
+      file_path = "#{Filezone::Engine.root}/app/assets/images/filezone/#{name}.svg"
+      return File.read(file_path).html_safe if File.exists?(file_path)
+      '(not found)'
+    end
   end
 end
--- lib/filezone/engine.rb
+++ lib/filezone/engine.rb
@@ -1,5 +1,11 @@
 module Filezone
   class Engine < ::Rails::Engine
     isolate_namespace Filezone
+
+    config.before_initialize do
+      ActiveSupport.on_load :action_controller_base do
+        helper Filezone::Engine.helpers
+      end
+    end
   end
 end
